5501c0abb5cb519538ae5c37aa42cfae8a04badf
Gabriel Bourget <gabrielbourget@gmail.com>
2019-04-21 20:48:38 -0400

    Working application, end of module 39

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -1,20 +1,52 @@
 import React from 'react';
+import { Query, Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import CartStyles from './styles/CartStyles';
 import Supreme from './styles/Supreme';
 import CloseButton from './styles/CloseButton';
 import SickButton from './styles/SickButton';
 
+export const LOCAL_STATE_QUERY = gql`
+	query {
+		cartOpen @client
+	}
+`;
+
+export const TOGGLE_CART_MUTATION = gql`
+	mutation{
+		toggleCart @client
+	} 
+`;
+
 const Cart = () => (
-	<CartStyles open={ true }>
-		<CloseButton title='close'>&times;</CloseButton>
-		<Supreme>Your Cart</Supreme>
-		<p>You have __ items in your cart.</p>
-		<footer>
-			<p>$10.10</p>
-			<SickButton>Checkout</SickButton>
-		</footer>
-	</CartStyles>
+	<Mutation mutation={ TOGGLE_CART_MUTATION }>
+		{
+			(toggleCart) => (
+				<Query query={ LOCAL_STATE_QUERY }>
+					{
+						({ data }) => (
+							<CartStyles open={ data.cartOpen }>
+								<CloseButton 
+									title='close'
+									onClick={ toggleCart }
+								>
+									&times;
+								</CloseButton>
+								<Supreme>Your Cart</Supreme>
+								<p>You have __ items in your cart.</p>
+								<footer>
+									<p>$10.10</p>
+									<SickButton>Checkout</SickButton>
+								</footer>
+							</CartStyles>
+						)
+					}
+				</Query>
+			)
+		}
+		
+	</Mutation>
 );
 
 export default Cart;
